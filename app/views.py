e1a90322abdd80d612aeb0b5e126425ee2749a91
richardgooden <richard.gooden5@gmail.com>
2017-03-28 13:46:04 +0000

    fixes

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ from app.models import UserProfile
 from models import UserProfile
 import datetime
 from forms import ProfileForm 
+from flask import jsonify
 
 @app.route("/")
 def newprofile():
@@ -40,25 +41,58 @@ def addprofile():
     
     return render_template(url_for('profiles'))
     
-@app.route("/profiles")
+@app.route("/profiles", methods=['GET', 'POST'])
 def profiles():
-    users = db.session.query(UserProfile).all()
-    return render_template('profiles.html',users=users)
     
-@app.route("/profile/<userid>")
+    if request.headers.get('Content-Type') == "application/json" or request.method == 'POST':
+        users1 = db.session.query(UserProfile)
+        users = []
+        l =[]
+        for user in users1:
+            id = user.id
+            username = user.username
+            l = (username, id)
+            users.append(l)
+        return jsonify(users)
+    else:
+        users = db.session.query(UserProfile).all()
+        return render_template('profiles.html',users=users)
+    
+@app.route("/profile/<userid>", methods=['GET', 'POST'])
 def profile(userid):
-    user = db.session.query(UserProfile).filter_by(id=userid).one()
-    id = user.id
-    img = user.img
-    date = user.date
-    username = user.username
-    firstname = user.first_name
-    lastname = user.last_name
-    gender = user.gender
-    age = user.age
-    bio = user.bio
-    password = user.password
-    return render_template("profileuser.html",id=id, firstname=firstname, lastname=lastname, username=username, password=password, age=age, bio=bio, img=img, date=date, gender=gender)
-    
+    if request.headers.get('Content-Type') == "application/json" or request.method == 'POST':
+        user = db.session.query(UserProfile).filter_by(id=userid).one()
+        id = user.id
+        img = user.img
+        img = str(img)
+        l = list(img)
+        del(l[0])
+        img = "".join(l)
+        
+        date = user.date
+        username = user.username
+        gender = user.gender
+        age = user.age
+        user1 = [id,username,img,gender,age,date]
+        
+        return jsonify(user1)
+    else:
+        user = db.session.query(UserProfile).filter_by(id=userid).one()
+        id = user.id
+        img = user.img
+        img = str(img)
+        l = list(img)
+        del(l[0])
+        img = "".join(l)
+        date = user.date
+        username = user.username
+        firstname = user.first_name
+        lastname = user.last_name
+        gender = user.gender
+        age = user.age
+        bio = user.bio
+        password = user.password
+        return render_template("profileuser.html",id=id, firstname=firstname, lastname=lastname, username=username, password=password, age=age, bio=bio, img=img, date=date, gender=gender)
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port="8080")
